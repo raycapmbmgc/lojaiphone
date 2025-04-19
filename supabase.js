4eb5ad494a2f02f5aabc39a06fd8c887b3f5fdae
raycapmbmgc <139500594+raycapmbmgc@users.noreply.github.com>
2025-04-19 20:52:19 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,18 +1,18 @@
 // supabase.js
+require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
-const SUPABASE_URL = 'https://jcjrjoaopubhzvlokfeq.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjanJqb2FvcHViaHp2bG9rZmVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTAxMzEwMSwiZXhwIjoyMDYwNTg5MTAxfQ.B8W4BgSk0uhNjZ2AhuaRZhzsUp1WImnsUEntK7_ikEE';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
 let supabase;
 
 try {
-  supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
   if (!SUPABASE_URL || !SUPABASE_KEY) {
     throw new Error('❌ URL ou CHAVE do Supabase não configuradas.');
   }
 
+  supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
   console.log('✅ Conectado ao Supabase com sucesso!');
 } catch (error) {
   console.error('🚨 Erro ao conectar ao Supabase:', error.message);
